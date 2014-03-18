2b4bc942d7249a4147ef6b40dba9b4e7f9908bfc
Stephanie Robison <stephanie.robison@uvm.edu>
2014-03-18 09:34:12 -0400

    Update Deck.java
    Added getDirections() function that returns specific directions for each card.

--- Deck.java
+++ Deck.java
@@ -92,7 +92,58 @@ public class Deck {
 		public int getRank() {
 			return rank;
 	    }//getRank
+				public String getDirections() {
+			theRank = getRank();
+			
+			if (theRank == 0){
+				direction = "Move any one pawn from Start to a square occupied by any opponent, " +
+					"sending that pawn back to its own Start. If there are no pawns on the player's "+
+					"Start, or no opponent's pawns on any squares, the turn is lost. "+
+					"If an enemy's pawn is swapped while it is in front of your HOME, "+
+					"your pawn is switched EXACTLY where your enemy's pawn is, not at your HOME."								
+			if( theRank == 1){
+				direction = "Move a pawn from Start or move a pawn 1 space forward.";
+			}
+			if( theRank == 2){
+				direction = "Move a pawn from Start or move a pawn 2 spaces forward.";
+			}
+			if( theRank == 3){
+				direction = "Move a pawn 3 spaces forward.";
+			}
+			if(theRank == 4){
+				direction = "Move a pawn 4 spaces backward.";
+			}
+			if(theRank == 5){
+				direction = "Move a pawn 5 spaces forward.";
+			}
+			if(theRank == 7){
+				direction = "Move one pawn 7 spaces forward or split the 7 spaces between two pawns "+
+					"(such as four spaces for one pawn and three for another). This makes it possible "+
+					"for two pawns to enter Home on the same turn, for example. "+
+					"The 7 cannot be split into a 6 and 1 or a 5 and 2 for the purposes of "+
+					"moving out of Start. The entire seven spaces must be used one way or the other or the turn is lost."
+			}
+			if (theRank == 8){
+				direction = "Move a pawn 8 spaces forward.";
+			}
+			
+			if(theRank == 10){
+				direction = "Move a pawn 10 spaces forward or 1 space backward. If a player cannot go forward ten"
+			}
+			
+			if(theRank == 11){
+				direction = "Move 11 spaces forward or switch places with one opposing pawn. "+
+					"A player that cannot move 11 spaces is not forced to switch and instead can "+
+					"forfeit the turn."
+			}
+			if(theRank == 12){
+				direction = "Move a pawn 12 spaces forward.";
+			}
 
+			return direction;
+				
+			
+		}
 	        
 		public String toString() {
 			String cardString = "";
