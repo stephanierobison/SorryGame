e95b1611320e5fda9695fcdfe10a09137fd9b380
Jeremy Gould <jfgould@Jeremys-Mac-mini.local>
2014-04-25 10:35:23 -0400

    V1.0

--- BoardPanel.java
+++ BoardPanel.java
@@ -6,7 +6,12 @@ import java.util.*;
 import java.io.*;
 
 /**
-   The BoardPanel is 
+   The BoardPanel is a visual representation of the internal state of
+   the Board object. It is comprised of a XY grid each square of which
+   is a clickable hot spot which can represent a single Space (or none).
+   The various Pawns are drawn directly above the Space which they
+   occupy.
+   
 */
 
 
@@ -14,26 +19,25 @@ public class BoardPanel extends JPanel{
 //*************************************************************************
 // CONSTANTS
 //************************************************************************* 
+   /** Board width */
    private final int NUMBER_COLUMNS;
+   /** Board height */
    private final int NUMBER_ROWS;
       
+   /** Scaling factor for objects drawn in Spaces. ie Pawns, Highlighting,
+   etc. will be FILL_SCALE * 100% of the size of a space */
    private final static double FILL_SCALE = 0.75; // MUST BE > 0 and < 1
 
-   private final static Color DEFAULT_TOKEN_COLOR = Color.pink;
-   private final static Color VALID_TARGET_COLOR = Color.black;
+   /** The default color of space highlighting */
+   private final static Color VALID_TARGET_COLOR = Color.orange;
 
 
 
 //*************************************************************************
 // INSTANCE VARIABLES
 //************************************************************************* 
-
+   /** The board background image */
    private BufferedImage bgImg;
-    private BufferedImage r;
-     private BufferedImage b;
-      private BufferedImage y;
-       private BufferedImage g;
-
 
    /** 
       Main internal state variable. When NULL no pawn is selected
@@ -63,22 +67,24 @@ public class BoardPanel extends JPanel{
 //*************************************************************************
 // CONSTRUCTORS
 //*************************************************************************      
+   /**
+      @param Game game The Game MODEL this will be a VIEW for
+      
+      On instantiation the BoardPanel has nothing selected
+      and populates itself based on the size of provided game.
+   */   
    public BoardPanel(Game game){
       super();
-      selectedPawn = null;
+      selectedPawn = null; // nothing selected
       selectedSpace = null;
-      targetSpaces = new ArrayList<Space>();
+      targetSpaces = new ArrayList<Space>(); // empty to start
       this.game = game;
-      NUMBER_COLUMNS = game.getBoard().COLUMNS;
+      NUMBER_COLUMNS = game.getBoard().COLUMNS; // set by game
       NUMBER_ROWS = game.getBoard().ROWS;
       
       bgImg = null;
       try{
-         bgImg = ImageIO.read(new File("board.jpg"));
-         r = ImageIO.read(new File("red.jpg"));
-         b = ImageIO.read(new File("blue.jpg"));
-         y = ImageIO.read(new File("yellow.jpg"));
-         g = ImageIO.read(new File("green.jpg"));
+         bgImg = ImageIO.read(new File("board.jpg")); // load background file
       }
       catch (IOException e){
          System.out.println("COULD NOT READ BOARD PANEL IMAGES!");
@@ -86,78 +92,84 @@ public class BoardPanel extends JPanel{
       
    } 
 //-------------------------------------------------------------------------
+   /**
+      @param Pawn p
+      
+      Selects the indicated pawn. While a pawn is selected it may be
+      possible to move the selected pawn.
+   */
    public void selectPawn(Pawn p){
       selectedPawn = p;
-      //updateTargets(); 
    }// end of selectPawn(Pawn)
-   
-   
+//-------------------------------------------------------------------------
+   /**
+      Deselects pawn. Can no longer move but might be able to select
+      another pawn.
+   */   
    public void deselectPawn(){
       selectedPawn = null;
       //updateTargets();
    }
-   
+//-------------------------------------------------------------------------
+   /**
+      @return the currently selected Pawn, may be null if nothing selected
+   */
    public Pawn getSelectedPawn(){
       return selectedPawn;
    }
-   
+//-------------------------------------------------------------------------
+   /**
+      @return true if selectedPawn != null, false otherwise
+   */
    public boolean isPawnSelected(){
       boolean result = false;
       if (selectedPawn != null)
          result = true;
       return result;
    }
-   
-   //---------------------------
+//-------------------------------------------------------------------------
+   /**
+      @param Space s
+      
+      Selects the indicated space. A selected space indicates that an
+      ambiguity about how to move to that space has been detected.
+   */
    public void selectSpace(Space s){
       selectedSpace = s;
    }   
-   
+//-------------------------------------------------------------------------
+   /**
+      Deselects the selected space making it possible to move the selected
+      pawn elsewhere.
+   */   
    public void deselectSpace(){
       selectedSpace = null;
    }
-   
+//-------------------------------------------------------------------------
+   /**
+      @return the currently selected space
+   */
    public Space getSelectedSpace(){
       return selectedSpace;
    }
-   
+//-------------------------------------------------------------------------
+   /**
+      @return true if selectedSpace != null, false otherwise
+    */
    public boolean isSpaceSelected(){
       boolean result = false;
       if (selectedSpace != null)
          result = true;
       return result;
    }
-   
-   
-   //---------------------------
-   
-
-
 //-------------------------------------------------------------------------
-   /*
-   private void updateTargets(){ // update valid move targets for a newly selected pawn
-      int [] moves = game.getMoves();
-      Ruleset rules = game.getRules();
-      if ((moves != null) && (selectedPawn != null)){ // only do moves if available and pawn is selected
-         Space sourceSpace = selectedPawn.whereAmI();
-         ArrayList<Space> targetSpaces = new ArrayList<Space>();
-         for (int i = 0; i < moves.length; i++){
-            // get all valid move targets
-            targetSpaces.addAll(rules.getTargets(selectedPawn, sourceSpace, moves[i]));
-         } // should have list of targets now
-         
-         if (targetSpaces.size() > 0)
-            this.targetSpaces = targetSpaces;
-         else
-            this.targetSpaces = null;
-      
-      }
-      else
-         this.targetSpaces = null;
-  }*/
-        
-//-------------------------------------------------------------------------
+  /**
+      @param Graphics g
   
+      JPanel's paint method has been overidden to permit this view
+      to draw the various Pawns and highlighting that need to be 
+      visible above the drawn board background.
+  */
    public void	paintComponent(Graphics	g){
 		super.paintComponent(g);
 		
@@ -168,58 +180,14 @@ public class BoardPanel extends JPanel{
 		int boxHeight = panelY / NUMBER_ROWS;
 		//	resize to nearest	multiple	of	box widths & heights
 		setSize((boxWidth*NUMBER_COLUMNS)+1, (boxHeight	* NUMBER_ROWS)+1);
-
-		//	NEEDS	TO	CHANGE
-		setBackground(Color.gray);
 		
-      g.drawImage(bgImg,
+      g.drawImage(bgImg,   // make board image background
                 0,
                 0,
                 panelX,
                 panelY,
                 null);
 
-      
-      
-      // Draw all components in Z-Axis Order
-      
-      /*
-      //drawGrid(g); // TEST ONLY
-      //draw column dividers
-	   for(int i =	0;	i <= NUMBER_COLUMNS;	i++){
-		g.setColor(Color.blue);
-		g.drawLine(i *	boxWidth, 0,
-						i * boxWidth, panelY);
-	   }
-	
-	   //draw row dividers
-	   for(int i =	0;	i <= NUMBER_ROWS;	i++){
-   		g.setColor(Color.green);
-   		g.drawLine(0, i *	boxHeight,
-   						panelX, i *	boxHeight);
-      }
-      */
-      
-      //DRAW SPACES ????? TEST ONLY??? SEE HOW IT LOOKS....
-/*      ArrayList<Space> allSpaces = game.getBoard().getAllSpaces();
-      for (int i = 0; i < allSpaces.size(); i++){
-            int gridX = allSpaces.get(i).getX();
-            int gridY = allSpaces.get(i).getY();
-            int xLow = getLowPixelX(gridX);
-            int xHigh = getHighPixelX(gridX);  
-            int yLow = getLowPixelY(gridY);
-            int yHigh = getHighPixelY(gridY);
-            allSpaces.get(i).drawMe(g, xLow, yLow, xHigh, yHigh);         
-      }
-      
-  */    
-      // DO USING INSTANCE VARIABLES TO REDUCE ON THE FLY COMPUTATION?
-      	  
-
-      
-      //highlightSpace(g, 0 ,0); // TEST ONLY
-      
-      
       // If both a pawn and a space are selected then highlight them both
       if ((selectedPawn != null) && (selectedSpace != null)){
          highlightSpace(g, selectedPawn.whereAmI());
@@ -241,39 +209,16 @@ public class BoardPanel extends JPanel{
          }         
       
       }
-		//drawMoves(g);
   		drawTokens(g);       // PAWNS GO ON TOP
 	  
-	  //System.out.println("Paint	Component");
 		
 	}//end of paintcomponent(Graphics)
-
 //-------------------------------------------------------------------------
-
-
-//-------------------------------------------------------------------------
-   
-   /*
-   private void drawMoves(Graphics g){
-      //System.out.println(moves);
-      if ((targetSpaces != null) && (selectedPawn != null) ){ // only show moves if available and pawn is selected
-         //System.out.println(targetSpaces);
-         
-         //fill in target spaces
-         for (int i = 0; i < targetSpaces.size(); i++){
-            int gridX = targetSpaces.get(i).getX();
-            int gridY = targetSpaces.get(i).getY();
-            int xLow = getLowPixelX(gridX);
-            int xHigh = getHighPixelX(gridX);  
-            int yLow = getLowPixelY(gridY);
-            int yHigh = getHighPixelY(gridY);
-            g.setColor(VALID_TARGET_COLOR);
-            g.fillRect(xLow,yLow,xHigh - xLow,yHigh - yLow);
-         
-         }
-      }
-   }// end of drawMoves*/
-//-------------------------------------------------------------------------
+   /**
+      Cycles through all tokens on the board,
+      calling their drawme methods in the region
+      where their space resides.
+   */
    private void drawTokens(Graphics g){
       ArrayList<Token> tokens = game.getTokens();
       Token currentToken;
@@ -281,7 +226,7 @@ public class BoardPanel extends JPanel{
       int gridY, yLow, yHigh;
       for (int i = 0; i < tokens.size(); i++){
          currentToken = tokens.get(i);
-         gridX = currentToken.whereAmI().getX();
+         gridX = currentToken.whereAmI().getX(); // Grid Coords => Pixel Coords
          gridY = currentToken.whereAmI().getY();
          xLow = getLowPixelX(gridX);
          xHigh = getHighPixelX(gridX);  
@@ -290,28 +235,45 @@ public class BoardPanel extends JPanel{
          currentToken.drawMe(g, xLow, yLow, xHigh, yHigh, FILL_SCALE);  
       }      
       
-   } // end drawPawns
+   } // end drawTokens
+//-------------------------------------------------------------------------
+   /** 
+      @param Graphics g
+      @param Space s
    
-   
+      Highlights Space s by calculating its grid coordinates and calling
+      drawme(Graphics, int, int)
+   */
    private void highlightSpace(Graphics g, Space s){
       highlightSpace(g, s.getX(), s.getY());
    }
-   
+//-------------------------------------------------------------------------
+   /**
+      @param Graphics g
+      @param int x
+      @param int y
+      
+      Highlights a space by drawing a boldly colored square
+      inside it's grid X,Y
+   */   
    private void highlightSpace(Graphics g, int x, int y){
       int xLow = getLowPixelX(x);
-      int xHigh = getHighPixelX(x);  
+      int xHigh = getHighPixelX(x);       // Grid XY => Pixel XY
       int yLow = getLowPixelY(y);
       int yHigh = getHighPixelY(y);
-      g.setColor(VALID_TARGET_COLOR);                   // NEED TO CHANGE
+      g.setColor(VALID_TARGET_COLOR);                  
       Graphics2D g2 = (Graphics2D) g;
-      int k = 6;
+      int k = 6;                       // set fat pen
       g2.setStroke(new BasicStroke(k));
-      g2.drawRect(xLow + k ,yLow + k,xHigh - xLow - (2*k),yHigh - yLow - (2*k));      
+      g2.drawRect(xLow + k ,yLow + k,xHigh - xLow - (2*k),yHigh - yLow - (2*k));   // actual draw op    
       
    }
-//-------------------------------------------------------------------------
-//GRID STUFF---------------------------------------------------------------
-
+//*************************************************************************
+// UTILITY FUNCTIONS
+//*************************************************************************
+/* The following functions are used to automate several straightforward
+   mappings of Grid XY => Pixel XY. */
+//*************************************************************************
    public void getGridCoords(int x, int y){
       // Zero index grid of boxes divying up panel evenly
       // in NUMBER_COLUMNS & NUMBER_ROWS
@@ -323,21 +285,21 @@ public class BoardPanel extends JPanel{
       int rowNumber = (y / boxHeight);
       System.out.println("Bow Column, Row = " + columnNumber + ", " + rowNumber);
    }
-   
+//-------------------------------------------------------------------------
    public int getGridX(int x){
       int panelX = this.getWidth();
       int boxWidth = panelX / NUMBER_COLUMNS;
       int columnNumber = (x / boxWidth);
       return columnNumber;
    }
-   
+//-------------------------------------------------------------------------
    public int getGridY(int y){
       int panelY = this.getHeight();
       int boxHeight = panelY / NUMBER_ROWS;
       int rowNumber = (y / boxHeight);
       return rowNumber;
    }
-   
+//-------------------------------------------------------------------------
    // grid coords -> pixel coords
    public int getLowPixelX(int x){
       int panelX = this.getWidth();
@@ -345,21 +307,21 @@ public class BoardPanel extends JPanel{
       //int columnNumber = (x / boxWidth);
       return boxWidth * x;      
    }
-
+//-------------------------------------------------------------------------
    public int getHighPixelX(int x){
       int panelX = this.getWidth();
       int boxWidth = panelX / NUMBER_COLUMNS;
       //int columnNumber = (x / boxWidth);
       return boxWidth * (x + 1);      
    }
-   
+//-------------------------------------------------------------------------
    public int getLowPixelY(int y){
       int panelY = this.getHeight();
       int boxHeight = panelY / NUMBER_ROWS;
       //int rowNumber = (y / boxHeight);
       return boxHeight * y;
    }
-   
+//-------------------------------------------------------------------------
    public int getHighPixelY(int y){
       int panelY = this.getHeight();
       int boxHeight = panelY / NUMBER_ROWS;
